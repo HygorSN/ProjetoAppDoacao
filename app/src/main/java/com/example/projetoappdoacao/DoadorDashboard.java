1c542276179a40014e6d07ae15dcec3ff112ccb1
HygorSN <hygorsilva131215@gmail.com>
2025-05-12 21:27:00 -0300

    primeira versão

---
+++ app/src/main/java/com/example/projetoappdoacao/DoadorDashboard.java
@@ -0,0 +1,12 @@
+package com.example.projetoappdoacao;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DoadorDashboard extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doador_dashboard);
+    }
+}
